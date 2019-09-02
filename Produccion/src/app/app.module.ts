74aea5bfea580c32900bc89493a5d4043df279b9
Entx <sio.coque@yavirac.edu.ec>
2019-09-02 13:05:16 -0500

    ok

--- Produccion/src/app/app.module.ts
+++ Produccion/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { HeaderPrincipalComponent } from './pagina-principal/header-principal/header-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     InicioComponent,
-    PaginaPrincipalComponent
+    PaginaPrincipalComponent,
+    HeaderPrincipalComponent
   ],
   imports: [
     BrowserModule,

99cc0c0fec1cafb702f24d21dea1e0d8e77c0625
Entx <sio.coque@yavirac.edu.ec>
2019-09-02 11:59:50 -0500

    ok

--- Produccion/src/app/app-routing.module.ts
+++ Produccion/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
    { path: 'login', component: LoginComponent },
    { path: 'inicio', component: InicioComponent },
+   { path: 'principal', component: PaginaPrincipalComponent },
+
 
 ];
 
--- Produccion/src/app/app.module.ts
+++ Produccion/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    InicioComponent
+    InicioComponent,
+    PaginaPrincipalComponent
   ],
   imports: [
     BrowserModule,
